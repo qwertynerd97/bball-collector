ec58f023d41d41d32006ffb9fa0168ccf5104346
Unknown <pr4h6n@gmail.com>
2018-04-01 13:12:18 -0400

    User Collection
    Started working on viewing user's card collection

---
+++ app/src/main/java/com/example/app/baseballmessenger/CardListActivity.java
@@ -0,0 +1,19 @@
+package com.example.app.baseballmessenger;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by pr4h6n on 4/1/18.
+ */
+
+public class CardListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+    }
+}
