b7d3933b1b0896b34f79b997d171cc0126b4ff13
Elli Howard <qwertynerd97@gmail.com>
2018-04-07 19:53:52 -0400

    Added card images to card detail

--- app/src/main/java/com/example/app/baseballmessenger/CardDetailActivity.java
+++ app/src/main/java/com/example/app/baseballmessenger/CardDetailActivity.java
@@ -4,7 +4,10 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
@@ -14,7 +17,11 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 /**
  * Created by pr4h6n on 4/1/18.
@@ -37,6 +44,21 @@ public class CardDetailActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         data = getIntent().getExtras().getParcelable("card");
+
+        final ImageView pic = findViewById(R.id.cardImage);
+        final long ONE_MEGABYTE = 1024 * 1024;
+        data.imageRef().getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                pic.setImageBitmap(bitmap);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+            }
+        });
 
         TextView cardName = findViewById(R.id.cardName);
         cardName.setText(data.name);
